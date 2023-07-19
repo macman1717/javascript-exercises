6c5860e3e8cf68f9eec7bd2b0499fbbb95de0583
macman1717 <127078586+macman1717@users.noreply.github.com>
2023-07-19 11:31:18 -0400

    problems 06 - 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,17 @@
-const convertToCelsius = function() {
-};
+function convertToFahrenheit(tempInCelc){
+  let tempToFahren = 0;
+  tempToFahren = (tempInCelc * 9/5) + 32;
+  
+  return Number(tempToFahren.toFixed(1));
+}
 
-const convertToFahrenheit = function() {
-};
+function convertToCelsius(tempInFahr){
+  let tempToCelcius = 0;
+  tempToCelcius = (tempInFahr - 32) * (5/9);
+  
+  return Number(tempToCelcius.toFixed(1));
+}
+
 
 // Do not edit below this line
 module.exports = {
